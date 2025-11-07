dc49c11fb4e08d196c06c1846e568320bf16c503
Benes Claire <claire.benes@artfx.fr>
2025-11-07 17:17:28 +0100

    [init]

---
+++ Source/PhysicWaterShader/PhysicWaterShader.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "PhysicWaterShader.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, PhysicWaterShader, "PhysicWaterShader" );
